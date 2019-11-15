8acda67a2d86caac611a21cbb3d623b3261583f2
Regino P <regino5298@gmail.com>
2019-11-15 10:14:31 -0700

    added CharacterCard

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,15 @@
 import React from "react";
+import styled from "styled-components"
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+
+return(
+  <div>
+    <h2>{props.name}</h2>
+      <p>Species: {props.species}</p>
+      <p>Status: {props.status}</p>
+      <p>Location: {props.location}</p>
+  </div>
+
+)
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+import CharacterCard from "./CharacterCard";
+import axios from "axios";
+import {Link} from "react-router-dom";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
